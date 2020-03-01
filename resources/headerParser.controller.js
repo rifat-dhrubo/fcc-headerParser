8209ee7cdf6d750808a250281347937cef9d7dee
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-03-01 18:10:54 +0600

    minor changes for a hosting server requirements

--- resources/headerParser.controller.js
+++ resources/headerParser.controller.js
@@ -3,10 +3,10 @@ const headerInfo = (req, res) => {
 	const { rawHeaders } = req;
 
 	const index = rawHeaders.reduce((acc, value, index, array) => {
-		if (value === 'Accept-Language') {
+		if (value === 'Accept-Language' || value === 'accept-language') {
 			acc['language'] = array[index + 1];
 		}
-		if (value === 'User-Agent') {
+		if (value === 'User-Agent' || value === 'user-agent') {
 			acc['software'] = array[index + 1];
 		}
 		return acc;
